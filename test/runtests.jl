4c9ce146567a2644ada3e0fb19ac753220ad77b7
Simon Kornblith <simon@simonster.com>
2015-04-23 22:53:24 -0400

    Fix bug in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using NCS, Base.Test
 
 ncs = readncs(joinpath(dirname(@__FILE__), "TestFile.Ncs"))
 @test ncs.header == "######## Neuralynx\r\nTest File\r\n"
-@test ncs.samples == [-typemin(Int16):typemax(Int16);]
+@test ncs.samples == [typemin(Int16):typemax(Int16);]
 dt = 1/32000
 @test ncs.times == [0:dt:dt*512*128-dt;]
 @test length(ncs.times.ranges) == 1
